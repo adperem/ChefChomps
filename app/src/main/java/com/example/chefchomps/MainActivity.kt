7d166e9f80d8fa6849b030b41cd72256acfc2ce5
Adrian Perez <adperem@unirioja.es>
2025-03-11 18:18:03 +0100

    sd

--- app/src/main/java/com/example/chefchomps/MainActivity.kt
+++ app/src/main/java/com/example/chefchomps/MainActivity.kt
@@ -30,8 +30,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+
+
     Text(
         text = "Hello $name!",
         modifier = modifier

a74494e0db5c129473aa7767367815aafc48c08d
Adrian Perez <adperem@unirioja.es>
2025-03-18 18:49:38 +0100

    a

--- app/src/main/java/com/example/chefchomps/model/Recipe.kt
+++ app/src/main/java/com/example/chefchomps/model/Recipe.kt
@@ -1,22 +1,22 @@
 package com.example.chefchomps.model
 
-data class Receip(
+data class Recipe(
     val id: Int,
     val title: String,
     val image: String,
     val imageType: String,
     val servings: Int,
     val readyInMinutes: Int,
-    val cookingMinutes: Int,
-    val preparationMinutes: Int,
-    val license: String,
+    val cookingMinutes: Int?, // Nullable porque puede ser null en el JSON
+    val preparationMinutes: Int?, // Nullable porque puede ser null en el JSON
+    val license: String?, // Nullable porque puede ser null en el JSON
     val sourceName: String,
     val sourceUrl: String,
     val spoonacularSourceUrl: String,
     val healthScore: Double,
     val spoonacularScore: Double,
     val pricePerServing: Double,
-    val analyzedInstructions: List<String> = emptyList(), // Lista vacía por defecto
+    val analyzedInstructions: List<String> = emptyList(), // Lista de pasos como texto
     val cheap: Boolean,
     val creditsText: String,
     val cuisines: List<String> = emptyList(),
@@ -39,17 +39,4 @@ data class Receip(
     val extendedIngredients: List<Ingredient>,
     val summary: String,
     val winePairing: WinePairing
-
-
-
-
-
-
-
-
-
-
-
-
-
 )

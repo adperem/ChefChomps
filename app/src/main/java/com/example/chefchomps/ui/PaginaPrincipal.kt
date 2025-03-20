c050374c9e4e3a1bd8bc280a9e1e8cbc69b6d4cc
Ivmarin <55666908+IvanMarinUR@users.noreply.github.com>
2025-03-20 18:53:54 +0100

    20 3 clase

--- app/src/main/java/com/example/chefchomps/ui/PaginaPrincipal.kt
+++ app/src/main/java/com/example/chefchomps/ui/PaginaPrincipal.kt
@@ -1,35 +1,48 @@
 package com.example.chefchomps.ui
 
-import ChefChompsTema
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import com.example.chefchomps.R
 
 class PaginaPrincipal :ComponentActivity(){
@@ -37,40 +50,23 @@ class PaginaPrincipal :ComponentActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ChefChompsTema(){
-                Surface (modifier = Modifier.fillMaxSize())
-                {
-                    Welcome();
-                }
-            }}
+            Welcome();
+        }
     }
     /**
      * Pagina de inicio para la aplicacion
      * */
-    @Preview
     @Composable
-    fun Welcome(modifier:Modifier=Modifier ){
-        Scaffold(
-            topBar = {
-                Row(verticalAlignment=Alignment.CenterVertically){
-                    Image(painter = painterResource(R.drawable.chomper), contentDescription = "",modifier=modifier
-                        .size(150.dp))
-                    Text(text="CHEF CHOMPS", maxLines = 1, textAlign = TextAlign.Center,modifier=modifier .fillMaxWidth(), fontSize = 7.em, fontWeight = FontWeight.Bold)
-                }
-            },
-            modifier = modifier.padding(10.dp)
-
-        ){ innerPadding->
-            LazyColumn(modifier=modifier
-                .padding(innerPadding)
-                .fillMaxWidth()) {
-
+    fun Welcome(modifier:Modifier=Modifier.fillMaxWidth()){
+        Column(modifier=modifier
+            .fillMaxWidth()) {
+            Row{
+            Image(painter = painterResource(R.drawable.chomper), contentDescription = "",modifier=modifier)
+            Text(text="CHEF CHOMPS",modifier=modifier)
             }
 
 
-
         }
-
     }
 
 }
---
+++ app/src/main/java/com/example/chefchomps/ui/VerReceta.kt
@@ -0,0 +1,42 @@
+package com.example.chefchomps.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.chefchomps.R
+import com.example.chefchomps.model.Recipe
+
+class VerReceta : ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecetaVer(Recipe r);
+        }
+    }
+    /**
+     * Pagina de visualización de una receta
+     * */
+    @Composable
+    fun RecetaVer(rec: Recipe, modifier: Modifier = Modifier.fillMaxWidth()){
+        Column(modifier=modifier
+            .fillMaxWidth()) {
+            Row{
+                Image(painter = painterResource(R.drawable.chomper), contentDescription = "",modifier=modifier)
+                Text(text="CHEF CHOMPS",modifier=modifier)
+            }
+
+
+        }
+    }
+
+}

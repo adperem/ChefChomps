07d32834362840f6c16e8a67b9d15a780a5d58e6
pezhuzha <pezhuzha@unirioja.es>
2025-04-24 18:28:33 +0200

    Search en pagina principal esta, hay una clase search para listas de string

--- app/src/main/java/com/example/chefchomps/ui/PaginaPrincipal.kt
+++ app/src/main/java/com/example/chefchomps/ui/PaginaPrincipal.kt
@@ -22,22 +22,32 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,7 +55,9 @@ import androidx.compose.ui.unit.em
 import coil.compose.AsyncImage
 import com.example.chefchomps.R
 import com.example.chefchomps.logica.ApiCLient
+import com.example.chefchomps.logica.ApiCLient.Companion.autocompleteRecipes
 import com.example.chefchomps.logica.ApiCLient.Companion.getRandomRecipe
+import com.example.chefchomps.model.Recipe
 import kotlinx.coroutines.runBlocking
 
 /**
@@ -69,17 +81,20 @@ class PaginaPrincipal :ComponentActivity(){
      * @param modifier modificador que define comportamiento
      * @param uiState contiene todos los datos relacionados con la página principal
      * */
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
     @SuppressLint("NotConstructor")
     @Preview
     @Composable
     fun PaginaPrincipal(modifier:Modifier=Modifier,
-                uiState:ViewModelPaginaPrincipal=ViewModelPaginaPrincipal()
+                uiState:ViewModelPaginaPrincipal= ViewModelPaginaPrincipal()
     ){
-        var active by rememberSaveable { mutableStateOf(false) }
         uiState.updatelist(runBlocking { getRandomRecipe() } )
+        var text by remember { mutableStateOf("") }
+        val focusManager = LocalFocusManager.current
+        val textFieldFocusRequester = remember { FocusRequester() }
+        val state = rememberScrollState()
         Scaffold(
             topBar = {
+                Column{
                 Row(verticalAlignment=Alignment.CenterVertically){
                     Image(painter = painterResource(R.drawable.menuicon), contentDescription = "",
                         modifier=modifier
@@ -87,70 +102,53 @@ class PaginaPrincipal :ComponentActivity(){
                     )
                     Image(painter = painterResource(R.drawable.chomper), contentDescription = "",modifier=modifier
                         .size(100.dp))
+
                     Text(text="CHEF CHOMPS", maxLines = 1, textAlign = TextAlign.Center,modifier=modifier .fillMaxWidth(),
                         fontSize = 7.em, fontWeight = FontWeight.Bold)
+                }
+                Row(modifier = Modifier.fillMaxWidth()
+                    .verticalScroll(state)){
+                TextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Buscar") },
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            uiState.clear()
+                            uiState.updatelist(runBlocking {autocompleteRecipes(text)})
+                            focusManager.clearFocus()
+                            text=""
+                        }),
+                    modifier = Modifier.focusRequester(textFieldFocusRequester)
+                )
+                IconButton(onClick = {
+                    uiState.clear()
+                    uiState.updatelist(runBlocking {autocompleteRecipes(text)})
+                    focusManager.clearFocus()
+                    text=""
+                }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.lupa),
+                        contentDescription = "back icon",
+                    )
+                }}
                 }
             },
             modifier = modifier.padding(10.dp)
 
         ){
             innerPadding->
-            var q by rememberSaveable { mutableStateOf("") }
-            var list by rememberSaveable { mutableStateOf(ArrayList<String>()) }
-            SearchBar(
-                query = q,
-                onQueryChange = { q=it },
-                onSearch = { list.clear()
-                           },
-                active = active,
-                onActiveChange = { active=it },
-            ){
-                Column(Modifier.verticalScroll(rememberScrollState())) {
-                    list.forEach { result ->
-                        ListItem(
-                            headlineContent = { Text(text=result) },
-                            modifier = Modifier
-                                .clickable {q=result
-                                    active = false
-                                }
-                                .fillMaxWidth()
-                        )
-                    }
-            }
             LazyColumn(modifier=modifier
                 .padding(innerPadding)
                 .fillMaxWidth()) {
-                items(uiState.getlist()){
-                    aux->
-                    Row(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .border(2.dp, Color(0xCCCCCCCC), shape = RoundedCornerShape(5.dp))
-                    ){
-                        Column(
-
-                            modifier = modifier
-                                .fillMaxWidth()
-                        ){
-                            Text(text=aux.title,
-                                textAlign = TextAlign.Center,
-                                modifier = modifier
-                                    .fillMaxWidth()
-                                )
-                            Spacer(modifier = Modifier.size(10.dp))
-                            AsyncImage(model=aux.image,
-                                placeholder = painterResource(R.drawable.nofoodplaceholder),
-                                contentDescription = "Imagen de la receta",
-                                onLoading = { println("Cargando imagen...") },
-                                onSuccess = { println("Imagen cargada con éxito") },
-                                onError = { println("Error al cargar la imagen: ${it.result.throwable}") },
-                                modifier = modifier
-                                    .align(Alignment.CenterHorizontally)
-                                    .fillMaxSize()
-                            )
-
-                        }
-                }
+                items(
+                    items=uiState.getlist(),
+                    key={
+                        item->item.title
+                    }
+                ){
+                    aux->RowReceta(aux)
                 }
             }
 
@@ -160,4 +158,4 @@ class PaginaPrincipal :ComponentActivity(){
 
     }
 
-}}
+}
